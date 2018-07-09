37a59a45ea2429cbf25812649928f2d49d13cfe9
maciek1066 <maciek1066@gmail.com>
2018-07-09 17:35:05 +0200

    models done

--- websites/models.py
+++ websites/models.py
@@ -1,35 +1,30 @@
 from django.db import models
 
-# Website:
-#     url
-#     title
-#     meta_description
-#     alexa_rank
-#     category - FK WebsiteCategory
-#     date_added
-#     date_updated
+
+class WebsiteCategory(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=128)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(null=True)
+    count = models.IntegerField()
 
 
 class Website(models.Model):
     url = models.CharField(max_length=128)
     title = models.CharField(max_length=32)
-    meta_description = models.CharField(max_length=32)
+    meta_description = models.CharField(max_length=128)
     alexa_rank = models.IntegerField()
-    category = models.ForeignKey(WebsiteCategory)
+    category = models.ForeignKey(WebsiteCategory, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(null=True)
 
 
-class WebsiteCategory(models.Model):
-    pass
-#     name
-#     description
-#     date_added
-#     date_updated
-#     count
+class WebPage(models.Model):
+    website = models.ForeignKey(Website, on_delete=models.CASCADE)
+    URL = models.CharField(max_length=128, unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(null=True)
+    title = models.CharField(max_length=32)
+    meta_description = models.CharField(max_length=128)
 
-# WebPage:
-#     website - FK Website
-#     URL - unikalny
-#     date_added
-#     date_updated
-#     title
-#     meta_description
+
--- websites_app/settings.py
+++ websites_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'websites',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'websites_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '127.0.0.1',
+        'NAME': 'block_chain',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'project%0',
     }
 }
 
