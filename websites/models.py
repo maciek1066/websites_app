2c2558979a5cafc36686e435a3e6773e5f1b3a58
maciek1066 <maciek1066@gmail.com>
2018-07-12 13:52:54 +0200

    working on get_websites

--- websites/models.py
+++ websites/models.py
@@ -14,12 +14,12 @@ class WebsiteCategory(models.Model):
 
 class Website(models.Model):
     url = models.CharField(max_length=128)
-    title = models.CharField(max_length=32)
-    meta_description = models.CharField(max_length=128)
+    title = models.CharField(max_length=256)
+    meta_description = models.CharField(max_length=512)
     alexa_rank = models.IntegerField()
     category = models.ForeignKey(WebsiteCategory, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.url
@@ -29,11 +29,8 @@ class WebPage(models.Model):
     website = models.ForeignKey(Website, on_delete=models.CASCADE)
     URL = models.CharField(max_length=128, unique=True)
     date_added = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True, null=True)
     title = models.CharField(max_length=32)
     meta_description = models.CharField(max_length=128)
 
 
-class TestModel(models.Model):
-    website = models.CharField(max_length=128)
-    position = models.CharField(max_length=12)
--- websites/tasks.py
+++ websites/tasks.py
@@ -2,7 +2,7 @@ from celery import Celery, shared_task
 from zipfile import ZipFile
 import urllib.request
 from io import BytesIO
-from .models import TestModel
+from .models import WebsiteCategory, Website, WebPage
 import bs4
 from urllib.request import urlopen as u_req
 from bs4 import BeautifulSoup as soup
@@ -29,6 +29,17 @@ def get_websites(url):
                 page_html = u_client.read()
                 u_client.close()
                 page_soup = soup(page_html, "html.parser")
-                title = page_soup.head.title.text
+                if page_soup.find("title"):
+                    title = page_soup.find("title").text
+                else:
+                    title = "no title"
                 meta = page_soup.find("meta", {"name": "description"})
                 meta_description = meta["content"] if meta else "no meta description given"
+                category = WebsiteCategory.objects.get(name="test")
+                new_object = Website.objects.create(
+                    url=url,
+                    title=title,
+                    meta_description=meta_description,
+                    alexa_rank=position,
+                    category=category,
+                    )
